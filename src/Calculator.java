55159f5a8a0c5b85ce700a0b2751247bf2e990d5
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2012-01-04 19:04:52 -0200

    Fim+retro

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,19 +1,41 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class Calculator {
 
+	private static final String OPERATOR_MATCHER = "(\\+|\\-|\\*|\\/)";
+
 	public int eval(String formula) {
-		String[] operands = formula.split("(\\+|\\-|\\*|\\/)");
-		if(formula.contains("+")){
-			return Integer.parseInt(operands[0]) + Integer.parseInt(operands[1]);
-		}else if (formula.contains("-"))  {
-			return Integer.parseInt(operands[0]) - Integer.parseInt(operands[1]);
-		}else if (formula.contains("*"))  {
-			return Integer.parseInt(operands[0]) * Integer.parseInt(operands[1]);
-		}else if (formula.contains("/"))  {
-			return Integer.parseInt(operands[0]) / Integer.parseInt(operands[1]);
-		}else {
-			return Integer.parseInt(formula);
+		
+		String[] tokens = formula.split(OPERATOR_MATCHER);
+		
+		String operator = "";
+		Matcher m = Pattern.compile(OPERATOR_MATCHER).matcher(formula);
+		while (m.find()) {
+			operator = m.group();
 		}
+		
+		Integer[] operands = new Integer[tokens.length];
+		for (int i = 0; i < tokens.length; ++i) {
+			operands[i] = Integer.parseInt(tokens[i]);
+		}
+		
+		return doOp(operator, operands);
+		
+	}
+
+	private int doOp(String operator, Integer[] operands) {
+		if(operator.equals("+") ){
+			return operands[0] + operands[1];
+		}else if (operator.equals("-"))  {
+			return operands[0] - operands[1];
+		}else if (operator.equals("*") )  {
+			return operands[0] * operands[1];
+		}else if (operator.equals("/"))  {
+			return operands[0] / operands[1];
+		}
+		return operands[0];
 	}
 
 }
