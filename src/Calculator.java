09c5a8aa137d3b0a4b300a1f13bb32ac940bd671
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2012-01-04 18:07:34 -0200

    finalizando as 4 operações básicas

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,11 +2,15 @@
 public class Calculator {
 
 	public int eval(String formula) {
-		String[] operands = formula.split("(\\+|\\-)");
+		String[] operands = formula.split("(\\+|\\-|\\*|\\/)");
 		if(formula.contains("+")){
 			return Integer.parseInt(operands[0]) + Integer.parseInt(operands[1]);
 		}else if (formula.contains("-"))  {
 			return Integer.parseInt(operands[0]) - Integer.parseInt(operands[1]);
+		}else if (formula.contains("*"))  {
+			return Integer.parseInt(operands[0]) * Integer.parseInt(operands[1]);
+		}else if (formula.contains("/"))  {
+			return Integer.parseInt(operands[0]) / Integer.parseInt(operands[1]);
 		}else {
 			return Integer.parseInt(formula);
 		}
--- src/_StringCalculatorTest.java
+++ src/_StringCalculatorTest.java
@@ -1,34 +1,51 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class _StringCalculatorTest {
 
+	private Calculator calc;
+	
+	@Before
+	public void setUp(){
+		calc = new Calculator();
+	}
+	
 	@Test
 	public void returnsTheEvalOfASimpleNumber(){
-		Calculator c = new Calculator();
-		assertEquals(1,c.eval("1"));
-		assertEquals(4,c.eval("4"));
-		assertEquals(60,c.eval("60"));
+		assertEquals(1,calc.eval("1"));
+		assertEquals(4,calc.eval("4"));
+		assertEquals(60,calc.eval("60"));
 	}
 	
 	@Test
 	public void evaluatesSumExpressions(){
-		Calculator c = new Calculator();
-		assertEquals(2,c.eval("1+1"));
-		assertEquals(8,c.eval("4+4"));
-		assertEquals(120,c.eval("60+60"));
+		assertEquals(2,calc.eval("1+1"));
+		assertEquals(8,calc.eval("4+4"));
+		assertEquals(120,calc.eval("60+60"));
 	}
 	
 	@Test
 	public void evaluatesSubExpressions(){
-		Calculator c = new Calculator();
-		assertEquals(0,c.eval("1-1"));
-		assertEquals(0,c.eval("60-60"));
-		assertEquals(-50,c.eval("50-100"));
+		assertEquals(0,calc.eval("1-1"));
+		assertEquals(0,calc.eval("60-60"));
+		assertEquals(-50,calc.eval("50-100"));
 	}
 	
+	@Test
+	public void evaluatesMultiplicationExpressions(){
+		assertEquals(2,calc.eval("1*2"));
+		assertEquals(36,calc.eval("6*6"));
+		assertEquals(5000,calc.eval("50*100"));
+	}
 	
+	@Test
+	public void evaluatesDivisionExpressions(){
+		assertEquals(2,calc.eval("4/2"));
+		assertEquals(1,calc.eval("6/6"));
+		assertEquals(2,calc.eval("100/50"));
+	}
 	
 }
